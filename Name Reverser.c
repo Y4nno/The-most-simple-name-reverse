6317b29d2256b300c02b3f0fda89d48486a2cd7e
Yanno <Norjanarradaza@gmail.com>
2024-06-16 23:28:02 +0800

    Add files via upload

---
+++ Name Reverser.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void Naming(char* name);
+
+int main(){
+
+    char name[50];
+
+    printf("Pls enter a name to reverse: ");
+    scanf("%s", name);
+
+    Naming(name);
+
+    return 0;
+}
+
+void Naming(char* name){
+
+    int size = sizeof(name);
+
+    for(int i = 0;  i <= size; i++){
+        printf("%c ", name[size - i]);
+    }
+}
